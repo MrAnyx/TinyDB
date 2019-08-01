bb236aaad287e317248c09c4bc96bc5b117e0440
MrAnyx <1ptitigre@gmail.com>
2019-08-01 14:42:22 +0200

    [fix] update composer.json

--- src/TinyDB.php
+++ src/TinyDB.php
@@ -3,8 +3,18 @@
 namespace MrAnyx\TinyDB;
 
 class TinyDB{
-    public function show(){
-        echo "show";
 
+	private $file;
+
+    public function open($file){
+    	if(!file_exists($file)){
+    		throw new Exception("File doesn't exists");
+    	}else{
+    		$this->file = $file;
+    	}
+    }
+
+    public function read(){
+    	return file_get_contents($this->file);
     }
 }
