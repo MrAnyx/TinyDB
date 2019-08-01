0055ed1462ce27fbda34cd6fcf780c207c8cb1a3
MrAnyx <1ptitigre@gmail.com>
2019-08-01 14:56:39 +0200

    [add] file supported

--- src/TinyDB.php
+++ src/TinyDB.php
@@ -7,10 +7,15 @@ class TinyDB{
 	private $file;
 
     public function open($file){
+
     	if(!file_exists($file)){
     		throw new Exception("File doesn't exists");
     	}else{
-    		$this->file = $file;
+            if(pathinfo($file)['extension'] != "json"){
+                throw new Exception("File not supported (Allowed : Json");
+            }else{
+                $this->file = $file;
+            }
     	}
     }
 
